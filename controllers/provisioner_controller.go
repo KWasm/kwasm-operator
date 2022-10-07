bbba434041323a4eebc07668826d7efde1403ba9
Max Schmidt <ma.schmidt@reply.de>
2022-10-07 18:24:45 +0200

    Add JobStatus Function
    Signed-off-by: Sven Pfennig <s.pfennig@reply.de>

--- controllers/provisioner_controller.go
+++ controllers/provisioner_controller.go
@@ -25,12 +25,10 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	ref "k8s.io/client-go/tools/reference"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	wasmv1alpha1 "github.com/kwasm/kwasm-provisioner-operator/api/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -75,53 +73,6 @@ const (
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.12.2/pkg/reconcile
 func (r *ProvisionerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.With().Str("node", req.Name).Logger()
-
-	var activeJobs []*batchv1.Job
-	var successfulJobs []*batchv1.Job
-	var failedJobs []*batchv1.Job
-	var childJobs batchv1.JobList
-	var mostRecentTime *time.Time
-
-	if err := r.List(ctx, &childJobs, client.MatchingLabels{"job-name": req.Name + "-control-plane-provision-wasm"}); err != nil {
-		log.Err(err).Msg("unable to list child Jobs")
-		return ctrl.Result{}, err
-	}
-	for i, job := range childJobs.Items {
-		_, finishedType := r.isJobFinished(&job)
-		switch finishedType {
-		case "": // ongoing
-			activeJobs = append(activeJobs, &childJobs.Items[i])
-		case batchv1.JobFailed:
-			failedJobs = append(failedJobs, &childJobs.Items[i])
-		case batchv1.JobComplete:
-			successfulJobs = append(successfulJobs, &childJobs.Items[i])
-		}
-
-		// We'll store the launch time in an annotation, so we'll reconstitute that from
-		// the active jobs themselves.
-		scheduledTimeForJob, err := r.getScheduledTimeForJob(&job)
-		if err != nil {
-			log.Err(err).Msg("unable to parse schedule time for child job " + job.Name)
-			continue
-		}
-		if scheduledTimeForJob != nil {
-			if mostRecentTime == nil {
-				mostRecentTime = scheduledTimeForJob
-			} else if mostRecentTime.Before(*scheduledTimeForJob) {
-				mostRecentTime = scheduledTimeForJob
-			}
-		}
-	}
-	for _, activeJob := range activeJobs {
-		jobRef, err := ref.GetReference(r.Scheme, activeJob)
-		if err != nil {
-			log.Err(err).Msgf("unable to make reference to active job %s", activeJob)
-			continue
-		}
-		log.Info().Msg(jobRef.Name)
-	}
-	log.Info().Msgf("job count for active jobs: %d failed jobs: %d successful Jobs: %d", len(activeJobs), len(failedJobs), len(successfulJobs))
-
 	node := &corev1.Node{}
 	r.Get(ctx, req.NamespacedName, node)
 	if err := r.Get(ctx, req.NamespacedName, node); err != nil {
@@ -138,9 +89,6 @@ func (r *ProvisionerReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	/*
 	   Step 1: Add or remove the label.
 	*/
-	provisioner := wasmv1alpha1.Provisioner{}
-
-	log.Info().Msgf("PROVISIONER: %s", provisioner.Name)
 
 	labelShouldBePresent := node.Annotations[addKWasmNodeLabelAnnotation] == "true"
 	labelIsPresent := node.Labels[nodeNameLabel] == node.Name
@@ -148,7 +96,7 @@ func (r *ProvisionerReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	if labelShouldBePresent == labelIsPresent {
 		// The desired state and actual state of the Pod are the same.
 		// No further action is required by the operator at this moment.
-		log.Info().Msg("no update required")
+
 		return ctrl.Result{}, nil
 	}
 	if labelShouldBePresent {
@@ -157,7 +105,7 @@ func (r *ProvisionerReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 			node.Labels = make(map[string]string)
 		}
 		node.Labels[nodeNameLabel] = node.Name
-
+		log.Info().Msgf("Trying to Deploy on %s", node.Name)
 		dep := r.deployJob(node, req)
 		err := r.Create(ctx, dep)
 		if err != nil {
@@ -165,7 +113,6 @@ func (r *ProvisionerReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 			return ctrl.Result{}, err
 		}
 
-		log.Info().Msg("Creating Job for " + node.Name)
 	} else {
 		// If the label should not be set but is, remove it.
 		delete(node.Labels, nodeNameLabel)
@@ -176,7 +123,7 @@ func (r *ProvisionerReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 				Name:      req.Name + "-provision-wasm",
 				Namespace: "kwasm-provisioner-operator-system",
 			},
-		})
+		}, client.PropagationPolicy(metav1.DeletePropagationBackground))
 		if err != nil {
 			log.Err(err).Msg("Failed to delete Job for " + req.Name)
 		}
@@ -184,11 +131,14 @@ func (r *ProvisionerReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 
 	if err := r.Update(ctx, node); err != nil {
 		if apierrors.IsConflict(err) {
+			log.Info().Msg("Node IsConflict, Requeuing")
 			// The Pod has been updated since we read it.
 			// Requeue the Pod to try to reconciliate again.
 			return ctrl.Result{Requeue: true}, nil
 		}
 		if apierrors.IsNotFound(err) {
+			log.Info().Msg("Node IsNotFound, Requeuing")
+
 			// The Pod has been deleted since we read it.
 			// Requeue the Pod to try to reconciliate again.
 			return ctrl.Result{Requeue: true}, nil
@@ -247,59 +197,29 @@ func (r *ProvisionerReconciler) deployJob(n *corev1.Node, req ctrl.Request) *bat
 		},
 	}
 	ctrl.SetControllerReference(n, dep, r.Scheme)
-	dep.SetOwnerReferences(n.GetOwnerReferences())
 	return dep
 }
-
-func (r *ProvisionerReconciler) isJobFinished(job *batchv1.Job) (bool, batchv1.JobConditionType) {
-	for _, c := range job.Status.Conditions {
-		if (c.Type == batchv1.JobComplete || c.Type == batchv1.JobFailed) && c.Status == corev1.ConditionTrue {
-			return true, c.Type
-		}
-	}
-	return false, ""
-}
-
-func (r *ProvisionerReconciler) getScheduledTimeForJob(job *batchv1.Job) (*time.Time, error) {
-	timeRaw := job.Annotations[scheduledTimeAnnotation]
-	if len(timeRaw) == 0 {
-		return nil, nil
-	}
-	timeParsed, err := time.Parse(time.RFC3339, timeRaw)
-	if err != nil {
-		return nil, err
-	}
-	return &timeParsed, nil
-}
-
-// func (r *ProvisionerReconciler) fetchNode(nodeName string) (*corev1.Node, error) {
-
-// 	if err != nil && errors.IsNotFound(err) {
-// 		r.logger.Error(err, "Node cannot be found", "nodeName", nodeName)
-// 		return nil, err
-// 	} else if err != nil {
-// 		r.logger.Error(err, "Failed to get node", "nodeName", nodeName)
-// 		return nil, err
-// 	}
-// 	return node, nil
-// }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ProvisionerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 
-	// set up a real clock, since we're not in a test
-	if r.Clock == nil {
-		r.Clock = realClock{}
-	}
-	if err := mgr.GetFieldIndexer().IndexField(context.Background(), &batchv1.Job{}, ".metadata.name.", func(rawObj client.Object) []string {
+	if err := mgr.GetFieldIndexer().IndexField(context.Background(), &batchv1.Job{}, ".metadata.name", func(rawObj client.Object) []string {
 		// grab the job object, extract the owner...
+
 		job := rawObj.(*batchv1.Job)
 		owner := metav1.GetControllerOf(job)
 		if owner == nil {
 			return nil
 		}
-		log.Info().Msg(owner.Name)
-		// ...and if so, return it
+		_, finishedType := r.isJobFinished(job)
+		switch finishedType {
+		case "": // ongoing
+			log.Info().Msgf("Job %s is still Ongoing", job.Name)
+		case batchv1.JobFailed:
+			log.Info().Msgf("Job %s is still failing...", job.Name)
+		case batchv1.JobComplete:
+			log.Info().Msgf("Job %s is Completed. Happy WASMing", job.Name)
+		}
 		return []string{owner.Name}
 	}); err != nil {
 		return err
@@ -309,4 +229,14 @@ func (r *ProvisionerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		For(&corev1.Node{}).
 		Owns(&batchv1.Job{}).
 		Complete(r)
+}
+
+func (r *ProvisionerReconciler) isJobFinished(job *batchv1.Job) (bool, batchv1.JobConditionType) {
+	for _, c := range job.Status.Conditions {
+		if (c.Type == batchv1.JobComplete || c.Type == batchv1.JobFailed) && c.Status == corev1.ConditionTrue {
+			return true, c.Type
+		}
+	}
+
+	return false, ""
 }
